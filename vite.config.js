dbac0ba23fbee01d052fd9416fd2252f98bfeb1b
Zer0Krypt <jonathanlundy88@gmail.com>
2025-03-18 04:26:26 -0500

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
-    server: {
-      port: 3000,
-      open: true // This will automatically open the browser
-    }
+  server: {
+    port: 3000,
+    open: false // Disable automatic browser opening
+  }
   }
